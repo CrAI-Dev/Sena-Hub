6f30b93ff042abda73fd710c6fcd294b33ea750f
CrAI-Dev <80041769+CrAI-Dev@users.noreply.github.com>
2022-09-24 21:35:56 +0700

    Update Loading.lua

--- CodeFolder/Project/Code/NotACode/ThisisScript/Script/main/Loading.lua
+++ CodeFolder/Project/Code/NotACode/ThisisScript/Script/main/Loading.lua
@@ -1,1 +1,12 @@
-local main = {}
+print("SenaHUB ON THE LOW!")
+local hub = {
+  [4954752502] = {
+    [1] = "https://raw.githubusercontent.com/CrAI-Dev/Zenicen_Hub/main/Tower%20of%20Misery.lua",
+  }
+  
+for i,v in pairs(hub) do
+    if i == game.PlaceId then
+      loadstring(game:HttpGet(v[1]))()
+     else
+      end
+    end
